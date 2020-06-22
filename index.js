781395f18dbc273925aed4478ef59ca86474ea67
olawale micheal juwon <52759812+olawalejuwonm@users.noreply.github.com>
2020-06-22 18:10:31 +0100

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //const baseUrl = 'http://localhost:3000/'
-const baseUrl = 'https://raw.githubusercontent.com/olawalejuwonm/LeaderBoard/master/leaders.json'
+const baseUrl = 'https://raw.githubusercontent.com/olawalejuwonm/LeaderBoard/master/'
 
 
 fetch(baseUrl + 'leaders.json')
